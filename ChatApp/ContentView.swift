014ab7ebcc2c2def1764176ad499f2066d9a64bc
hyeonseongkang <gt9697@naver.com>
2021-08-19 22:47:10 +0900

    Initial Commit

---
+++ ChatApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChatApp
+//
+//  Created by 강현성 on 2021/08/19.
+//  Copyright © 2021 Mirror. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
