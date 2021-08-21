3f51f68748e830a0146e11f7c439e4db28f00882
hyeonseongkang <gt9697@naver.com>
2021-08-21 22:28:38 +0900

    ImagePicker.swift추가

--- ChatApp/ContentView.swift
+++ ChatApp/ContentView.swift
@@ -16,11 +16,24 @@ struct ContentView: View {
     var body: some View {
             
         Button(action: {
-            self.ref.childByAutoId().setValue(["name" :"hyeonseong", "age" : 25])
+ 
+    
+            
+            //self.ref.childByAutoId().setValue(["name" :"hyeonseong", "age" : 25])
         }) {
             Text("InputData")
         }
     
+    }
+    
+    func convertImageToBase64String (img: UIImage) -> String {
+        return img.jpegData(compressionQuality: 1)?.base64EncodedString() ?? ""
+    }
+    
+    func convertBase64StringToImage (imageBase64String:String) -> UIImage {
+    let imageData = Data.init(base64Encoded: imageBase64String, options: .init(rawValue: 0))
+    let image = UIImage(data: imageData!)
+    return image!
     }
 }
 
